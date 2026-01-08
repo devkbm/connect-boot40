04d197d7c92f9fd416921ae8346a17eb703cde28
devkbm <kbm0417@nate.com>
2026-01-08 18:07:32 +0900

    2026-01-08

--- src/main/java/com/like/system/company/application/port/in/select/CompanySelectDTOMapstruct.java
+++ src/main/java/com/like/system/company/application/port/in/select/CompanySelectDTOMapstruct.java
@@ -10,16 +10,10 @@ import org.mapstruct.factory.Mappers;
 
 import com.like.system.company.domain.CompanyInfo;
 
-@Mapper(
-	componentModel = ComponentModel.SPRING,
-	nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-    nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT,
-    unmappedTargetPolicy = ReportingPolicy.IGNORE,
-    unmappedSourcePolicy = ReportingPolicy.IGNORE
-)
+@Mapper(componentModel = ComponentModel.SPRING, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, nullValueMappingStrategy = NullValueMappingStrategy.RETURN_NULL, unmappedTargetPolicy = ReportingPolicy.IGNORE, unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface CompanySelectDTOMapstruct {
-	
-	CompanySelectDTOMapstruct INSTANCE = Mappers.getMapper( CompanySelectDTOMapstruct.class ); 
+
+	CompanySelectDTOMapstruct INSTANCE = Mappers.getMapper(CompanySelectDTOMapstruct.class);
 
 	@Mapping(target = "companyCode", source = "id.companyCode")
 	@Mapping(target = "clientAppUrl", ignore = true)
